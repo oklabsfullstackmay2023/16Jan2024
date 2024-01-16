b8e2cd12491b6bbceca60a9ded69981548316743
oklabsfullstackmay2023 <oklabsfullstackmay2023@gmail.com>
2024-01-16 15:49:16 +0530

    inheritance

---
+++ inheritance3.py
@@ -0,0 +1,60 @@
+class A():
+    #1. property/variable
+    name="Vishal"
+    surname="Mahawar"
+    pass
+class B(A):
+    pass
+class C(B):
+    pass
+class D(C):
+    pass
+class E(D):
+    pass
+class F(E):
+    pass
+class G(F):
+    pass
+class H(G):
+    pass
+class I(H):
+    pass
+class J(I):
+    pass
+class K(J):
+    pass
+class L(K):
+    pass
+class M(L):
+    pass
+class N(M):
+    pass
+class O(N):
+    pass
+class P(O):
+    pass
+class Q(P):
+    pass
+class R(Q):
+    pass
+class S(R):
+    pass
+class T(S):
+    pass
+class U(T):
+    pass
+class V(U):
+    pass
+class W(V):
+    pass
+class X(W):
+    pass
+class Y(X):
+    pass
+class Z(Y):
+    pass
+
+#2 create class external object is many time process
+ceo1=Z()
+print(ceo1.name)
+print(ceo1.surname)
